553e4f941dfa918935df7e27879e67e06c49705f
laminhvu <laminhvu1234@gmail.com>
2026-09-14 15:58:55 +0700

    API Session1 : add bai 4

--- api_session1/app.py
+++ api_session1/app.py
@@ -1,19 +1,31 @@
-from flask import Flask, jsonify, request
-from uuid import uuid4  
+from flask import Flask ,jsonify , request
 app = Flask(__name__)
-STUDENTS = []
-@app.route("/students", methods=["POST"])
-def create_student():
-    body = request.get_json(silent=True) or {}
-    name = body.get("name")
-    if not name:
-        return jsonify({"error":"name la bat buoc"}), 400
-    student = {
-        "id" :str(uuid4()),
-        "name": name,
-        "gpa" : body.get("gpa", 0.0),
-    }
-    STUDENTS.append(student)
-    return jsonify(STUDENTS), 201
+@app.route("/")
+def index():
+    return {"message": "Hello, APIIIII"}
+BOOKS = []
+def find_by_id(book_id):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            return book
+    return None
+@app.route("/books/<book_id>", methods = ["GET"])
+def get_book(book_id):
+    book = find_by_id(book_id)
+    if book is None:
+        return jsonify({"error" : "not found"}) , 404
+    return jsonify(book), 200
+
+@app.route("/items/<int:item_id>")
+def get_item(item_id):
+    return jsonify({"id": item_id}), 200
+
+@app.route("/books", methods = ["GET"])
+def list_book():
+    limit = int(request.args.get("limit", 20))
+    q = request.args.get("q","").strip().lower()
+    item = [b for b in BOOKS if q in b["t"].lower()]
+    return jsonify({"items" : item}), 200
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port = 5000 , debug = True)
