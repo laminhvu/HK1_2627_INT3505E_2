7c32b17e3a68eb5e046856bb580c72622f34a73a
laminhvu <laminhvu1234@gmail.com>
2026-09-14 15:54:40 +0700

    API Session1: Add bai 3

--- api_session1/app.py
+++ api_session1/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request
+from uuid import uuid4  
 app = Flask(__name__)
-@app.route("/health", methods = ["GET"])
-def health():
-    return jsonify({"status": "ok"}) , 200
-@app.route("/echo", methods = ["POST"])
-def echo():
-    data = request.get_json(silent= True) or{}
-    return jsonify({"you_sent" : data}), 200
+STUDENTS = []
+@app.route("/students", methods=["POST"])
+def create_student():
+    body = request.get_json(silent=True) or {}
+    name = body.get("name")
+    if not name:
+        return jsonify({"error":"name la bat buoc"}), 400
+    student = {
+        "id" :str(uuid4()),
+        "name": name,
+        "gpa" : body.get("gpa", 0.0),
+    }
+    STUDENTS.append(student)
+    return jsonify(STUDENTS), 201
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port = 5000, debug= True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
