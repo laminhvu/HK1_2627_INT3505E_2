b10281d48fe48730def73490d4161a135eaa5855
laminhvu <laminhvu1234@gmail.com>
2026-09-14 21:58:28 +0700

    btvn update bai 6

--- api_session1/app.py
+++ api_session1/app.py
@@ -1,13 +1,48 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 _next = 1
-BOOKS = [{"id": 1, "title" : "clean code", "author" : "lmv"}]
+BOOKS = [{"id": 1, "title" : "clean code", "author" : "lmv" , "year" : 2006}]
 def find(bid):
     return next((b for b in BOOKS if b["id"] == bid), None)
 #get
+def sort_title(books):
+    for i in range(len(books)):
+        for j in range(i + 1, len(books)):
+            if books[i]["title"].lower() > books[j]["title"].lower():
+                temp = books[i]
+                books[i] = books[j]
+                books[j] = temp
 @app.route("/books", methods = ["GET"])
 def list_book():
     n = int(request.args.get("limit",100))
+    q = request.args.get("q")
+    sort = request.args.get("sort")
+    year = int(request.args.get("year"))
+
+    if q:
+        result = []
+        for book in BOOKS:
+            if q in book["title"]:
+                result.append(book)
+        if sort == "title":
+            sort_title(result)
+        return jsonify(result[:n]), 200   
+
+    if sort == "title":
+        books = BOOKS
+        sort_title(books)
+        return jsonify(books[:n]), 200
+
+    if year:
+        result = []
+        for book in BOOKS:
+            if book["year"] >= year:
+                result.append(book)
+        return jsonify(result[:n]), 200
+
+
+
+    
     return jsonify(BOOKS[:n]), 200
 #get detail
 @app.route("/books/<int:bid>", methods= ["GET"])
@@ -16,6 +51,8 @@ def get_book(bid):
     if not book:
         return {"error" : "not found"} , 404
     return jsonify(book) , 200
+#get book theo q
+@app.route("/books/")
 
 #create
 @app.route("/books", methods = ["POST"])
